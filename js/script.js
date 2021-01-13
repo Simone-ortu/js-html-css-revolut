49e6fdbb8de1160c41eafebd9113379bdf0dc86c
Simone-Ortu <simo.28.09o@gmail.com>
2021-01-13 18:00:23 +0100

    Correzione

--- js/script.js
+++ js/script.js
@@ -1,32 +1,12 @@
 // Dropdownmenu 
 
 $(document).ready(function(){
-    $('.accounts').click(function(){
-        $('#account').toggleClass('active')
+
+    $('.has-dropdown').hover(function(){
+        $(this).children('.dropdown').toggleClass('active')
     });
-});
-$(document).ready(function(){
-    $('.prodotti').click(function(){
-        $('#prodotto').toggleClass('active')
-    });
-});
-$(document).ready(function(){
-    $('.azienda').click(function(){
-        $('#company').toggleClass('active')
-    });
-});
-$(document).ready(function(){
-    $('.aiuto').click(function(){
-        $('#help').toggleClass('active')
-    });
-});
-$(document).ready(function(){
-    $('.lingue').click(function(){
-        $('#lang').toggleClass('active')
-    });
+
+    
 });
 
-// $(document).click(function () {
-//     $('.dropdown').removeClass('active').eq($(this).index()).addClass('active');
-//   });
 
